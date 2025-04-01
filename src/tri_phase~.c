761c0e3449071b97422eec946ca4231c4c83bebb
scossar <scossar3@gmail.com>
2025-03-31 23:27:33 -0700

    fix inlets

--- src/tri_phase~.c
+++ src/tri_phase~.c
@@ -33,7 +33,6 @@ typedef struct _tri_phase
   t_float x_peak;
   t_float x_range;
 
-  t_inlet *in_1; // frequency
   t_inlet *in_2; // peak
   t_inlet *in_3; // phase
 } t_tri_phase;
@@ -114,6 +113,12 @@ static void tri_phase_hi(t_tri_phase *x, t_floatarg f)
   x->x_range = f - x->x_low;
 }
 
+static void tri_phase_free(t_tri_phase *x)
+{
+  inlet_free(x->in_2);
+  inlet_free(x->in_3);
+}
+
 static void *tri_phase_new(t_floatarg f)
 {
   t_tri_phase *x = (t_tri_phase *)pd_new(tri_phase_class);
@@ -127,9 +132,6 @@ static void *tri_phase_new(t_floatarg f)
   tri_phase_low(x, x->x_low);
   tri_phase_hi(x, x->x_hi);
 
-  x->in_1 = inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_signal, &s_signal);
-  pd_float((t_pd *)x->in_1, x->x_f);
-
   x->in_2 = inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_signal, &s_signal);
   pd_float((t_pd *)x->in_2, x->x_peak);
 
@@ -142,8 +144,13 @@ static void *tri_phase_new(t_floatarg f)
 
 void tri_phase_tilde_setup(void)
 {
-  tri_phase_class = class_new(gensym("tri_phase~"), (t_newmethod)tri_phase_new, 0,
-                                  sizeof(t_tri_phase), 0, A_DEFFLOAT, 0);
+  tri_phase_class = class_new(gensym("tri_phase~"),
+                              (t_newmethod)tri_phase_new,
+                              (t_method)tri_phase_free,
+                              sizeof(t_tri_phase),
+                              CLASS_DEFAULT,
+                              A_DEFFLOAT, 0);
+
   CLASS_MAINSIGNALIN(tri_phase_class, t_tri_phase, x_f);
   class_addmethod(tri_phase_class, (t_method)tri_phase_dsp,
                   gensym("dsp"), A_CANT, 0);
